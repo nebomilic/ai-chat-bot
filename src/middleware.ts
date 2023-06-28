3e8aa81539d1caf32ee67542727ae8317b3572a8
Nebo Milic <nebo.milic@gmail.com>
2023-06-28 10:05:12 +0200

    Add redirect from /my to /my/bot-data

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,16 @@
 import { authMiddleware } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
+
 export default authMiddleware({
   publicRoutes: ['/', '/sign-in'],
+  afterAuth(auth, req, evt) {
+    // always redirect from /my to /my/bot-data
+    if (req.nextUrl.pathname === '/my') {
+      return NextResponse.redirect(new URL('/my/bot-data', req.url))
+    }
+  },
 })
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+}
