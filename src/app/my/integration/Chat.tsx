07ca8c286c865b68e23f309bdacd5b0c184f5198
Nebo Milic <nebo.milic@gmail.com>
2023-07-03 22:52:01 +0200

    Change loading spinner color to indigo

--- src/app/my/integration/Chat.tsx
+++ src/app/my/integration/Chat.tsx
@@ -17,7 +17,7 @@ export default function Chat() {
           onChange={handleInputChange}
         />
       </form>
-      <div className="mt-3 sm:mt-5">
+      <div className="mt-6">
         {completion && (
           <div className="mt-2">
             <p className="text-sm text-gray-600">{completion}</p>
@@ -27,7 +27,7 @@ export default function Chat() {
         {isLoading && !completion && (
           <p className="flex items-center justify-center mt-4">
             <svg
-              className="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-900"
+              className="animate-spin -ml-1 mr-3 h-8 w-8 text-indigo-600"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -41,7 +41,7 @@ export default function Chat() {
                 stroke-width="4"
               ></circle>
               <path
-                className="opacity-75"
+                className="opacity-90"
                 fill="currentColor"
                 d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
               ></path>
