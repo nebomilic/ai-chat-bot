73c45cc0411d11d10debd9d78cbfbc0412edb7fd
Nebo Milic <nebo.milic@gmail.com>
2023-07-03 22:45:59 +0200

    Update chat text placeholder

--- src/app/my/integration/Chat.tsx
+++ src/app/my/integration/Chat.tsx
@@ -11,7 +11,7 @@ export default function Chat() {
     <div>
       <form onSubmit={handleSubmit}>
         <input
-          placeholder="Ask anything about Bane Kerac..."
+          placeholder="Ask anything about the text you entered..."
           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
           value={input}
           onChange={handleInputChange}
